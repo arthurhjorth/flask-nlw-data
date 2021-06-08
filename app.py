307cdfdbcbddfe161c79766bf56482eab4468087
arthur hjorth <arthur@mgmt.au.dk>
2021-06-08 13:27:26 +0200

    Now also version names in data collect template

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
 
-@app.route("/", methods=["POST", "GET"])
-def flask_nlw():
-    model_name = request.args.get('model', '', type=str)
-    print(f"serving {model_name}")
-    if model_name != '':
-        return render_template('data_collect.html', model_name=model_name)
-    return jsonify({})
+#@app.route("/", methods=["POST", "GET"])
+#def flask_nlw():
+#    model_name = request.args.get('model', '', type=str)
+#    print(f"serving {model_name}")
+#    if model_name != '':
+#        return render_template('data_collect.html', model_name=model_name)
+#    return jsonify({})
 
 
 @app.route("/model/<int:version>/<string:model_name>", methods=["POST", "GET"])
 def model_version(version, model_name):
     print(f"serving {model_name}")
     if version == 1:
-        return render_template('data_collect.html', model_name=model_name)
+        return render_template('data_collectV1.html', model_name=model_name)
     ## this is for future versions of NetLogoWeb which may nneeneed another template
     return jsonify({})
 
