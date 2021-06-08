86f0542fcec936cfa7ea10e8287a1c4744fd093c
arthur hjorth <arthur@mgmt.au.dk>
2021-06-08 12:18:37 +0200

    added summy submit_data endpoint

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def flask_nlw():
     model_name = request.args.get('model', '', type=str)
     print(f"serving {model_name}")
@@ -10,3 +10,9 @@ def flask_nlw():
         return render_template('data_collect.html', model_name=model_name)
     return jsonify({})
 
+
+@app.route("/submit_data", methods=["POST", "GET"])
+def submit_data():
+    data = request.args.to_dict()
+    print(data)
+    return jsonify({})
