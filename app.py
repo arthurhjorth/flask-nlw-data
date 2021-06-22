2b354f45027a459e674e4a735b9ff2f41d578eba
arthur hjorth <arthur@mgmt.au.dk>
2021-06-22 13:41:13 +0200

    removed commented out lines

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
-
-#@app.route("/", methods=["POST", "GET"])
-#def flask_nlw():
-#    model_name = request.args.get('model', '', type=str)
-#    print(f"serving {model_name}")
-#    if model_name != '':
-#        return render_template('data_collect.html', model_name=model_name)
-#    return jsonify({})
-
-
 @app.route("/model/<int:version>/<string:model_name>", methods=["POST", "GET"])
 def model_version(version, model_name):
     print(f"serving {model_name}")
