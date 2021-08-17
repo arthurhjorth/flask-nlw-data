343ca316394c1c312ce2c3fbdfc5b87c8f98dc1d
Arthur Hjorth <arthur@mgmt.au.dk>
2021-08-17 12:56:44 +0200

    jsonifying correctly

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ ur_data = json.load(open('secrets.json'))
 url = "https://staging.quickstorage.scienceathome.org/save"
 
 
+@app.route("/test/<string:model_name>", methods=["POST", "GET"])
+def test(model_name):
+    print(f"serving {model_name} on test")
+    return render_template('test.html', model_name=model_name)
+
+    ## this is for future versions of NetLogoWeb which may nneeneed another template
+    return jsonify({})
+
 @app.route("/model/<int:version>/<string:model_name>", methods=["POST", "GET"])
 def model_version(version, model_name):
     print(f"serving {model_name}")
@@ -28,7 +36,8 @@ def submit_data():
     print("received data")
     data = request.args.to_dict()
     data_dict = copy.deepcopy(ur_data)
-    data_dict['data'] = data
+    data_dict['data'] = json.loads(data['data'])
+    print(data_dict)
     r = requests.post(url = url, json = data_dict)
     print(r)
     return jsonify({})
